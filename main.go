f0fd2cf0b0eee9632d87e27043390952a5dcf1ac
david <d.mis09@gmail.com>
2018-08-09 12:07:46 -0500

    basic i18n ready

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+var msg = map[string]string{
+	"EN": "I love cookies",
+	"ES": "Yo quiero cookies",
+}
+
+var language string
+
+func init() {
+	language = "ES"
+}
+
 func main() {
-	fmt.Println("I love cookies!")
+	fmt.Println(msg[language])
 }
