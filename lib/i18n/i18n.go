432f5253f0de4a918b213779cbf610309b33d17c
david <d.mis09@gmail.com>
2018-08-10 15:51:52 -0500

    Nicer i18n

--- lib/i18n/i18n.go
+++ lib/i18n/i18n.go
@@ -1,27 +1,27 @@
-package lib
+package i18n
 
 import "github.com/pkg/errors"
 
 // Messages is an i18n dictionary. It's keys are string labels, and the values are
 // internationalized strings.
-type Messages map[string]string
+var Messages map[string]string
 
-// NewMessages populates an i18n dictionary. The 'language' parameter must be a 2-letter
+// UseLanguage populates an i18n dictionary. The 'language' parameter must be a 2-letter
 // ISO language code. In a real application, we would probably load the dictionary from
 // a file, but for this toy it's okay to just build the maps on the fly.
-func NewMessages(language string) (m Messages, err error) {
+func UseLanguage(language string) error {
 	switch language {
 	case "EN":
-		m = map[string]string{
+		Messages = map[string]string{
 			"main": "I love cookies",
 		}
 	case "ES":
-		m = map[string]string{
+		Messages = map[string]string{
 			"main": "Yo quierro cookies",
 		}
 	default:
-		return nil, errors.New("unsupported language " + language)
+		return errors.New("unsupported language " + language)
 	}
-
-	return m, nil
+	
+	return nil
 }
--- main.go
+++ main.go
@@ -3,28 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
-	. "github.com/davidMis/cookieMonster/lib"
+	"github.com/davidMis/cookieMonster/lib/i18n"
 )
 
-// language is the 2-letter ISO language code provided from the environment
-var language string
-
-// messages is our i18n dictionary
-var messages Messages
-
 func init() {
-	var ok bool
-	var err error
-
 	// Populate 'language' from the environment.
-	language, ok = os.LookupEnv("LANGUAGE")
+	language, ok := os.LookupEnv("LANGUAGE")
 	if !ok {
 		fmt.Println("LANGUAGE must be set")
 		os.Exit(1)
 	}
 
 	// Build i18n dictionary
-	messages, err = NewMessages(language)
+	err := i18n.UseLanguage(language)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -32,5 +23,5 @@ func init() {
 }
 
 func main() {
-	fmt.Println(messages["main"])
+	fmt.Println(i18n.Messages["main"])
 }
