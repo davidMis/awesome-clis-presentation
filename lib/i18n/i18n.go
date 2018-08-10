8690c0d4b697167e5346a8e96739ef4eed7e1ead
david <d.mis09@gmail.com>
2018-08-10 18:42:31 -0500

    App w/ some flags & multiple actions

--- lib/i18n/i18n.go
+++ lib/i18n/i18n.go
@@ -6,6 +6,7 @@ import "github.com/pkg/errors"
 // internationalized strings.
 var Messages map[string]string
 
+
 // UseLanguage populates an i18n dictionary. The 'language' parameter must be a 2-letter
 // ISO language code. In a real application, we would probably load the dictionary from
 // a file, but for this toy it's okay to just build the maps on the fly.
@@ -14,14 +15,20 @@ func UseLanguage(language string) error {
 	case "EN":
 		Messages = map[string]string{
 			"main": "I love cookies",
+			"eatCookie": "YUM! I ate a cookie!",
+			"eatOther": "YUCK! I ate a %s",
+			"prepareCookie": "Time to bake some cookies",
+			"prepareOther": "I don't know how to prepare %s",
 		}
 	case "ES":
 		Messages = map[string]string{
-			"main": "Yo quierro cookies",
+			"main": "",
+			"eatCookie": "",
+			"eatOther": "",
 		}
 	default:
 		return errors.New("unsupported language " + language)
 	}
-	
+
 	return nil
 }
--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"fmt"
 	"os"
 	"github.com/davidMis/cookieMonster/lib/i18n"
+	"flag"
 )
+
+var flagEat bool
+var flagPrepare bool
+var flagFood string
+var flagKind string
 
 func init() {
 	// Populate 'language' from the environment.
@@ -20,8 +26,41 @@ func init() {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+
+	// Define and parse flags
+	flag.BoolVar(&flagEat, "eat", false, "Perform action: Eat the food")
+	flag.BoolVar(&flagPrepare, "prepare", false, "Perform action: Prepare the food")
+	flag.StringVar(&flagFood, "food", "cookie", "What food to eat")
+	flag.StringVar(&flagKind, "kind", "chocolate chip", "What kind of cookie to eat")
+	flag.Parse()
 }
 
 func main() {
+	if flagEat {
+		eat(flagFood, flagKind)
+		return
+	}
+
+	if flagPrepare {
+		prepare(flagFood)
+		return
+	}
+
 	fmt.Println(i18n.Messages["main"])
+}
+
+func eat(food, kind string) {
+	if food == "cookie" {
+		fmt.Printf(i18n.Messages["eatCookie"], kind)
+	} else {
+		fmt.Printf(i18n.Messages["eatOther"], food)
+	}
+}
+
+func prepare(food string) {
+	if food == "cookie" {
+		fmt.Println(i18n.Messages["prepareCookie"])
+	} else {
+		fmt.Printf(i18n.Messages["prepareOther"], food)
+	}
 }
